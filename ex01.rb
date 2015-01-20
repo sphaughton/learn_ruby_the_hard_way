59ec188a432b3f9ed41781c65b4e1a781a5f9845
Sean Haughton <seanphaughton@gmail.com>
2015-01-20 12:17:12 +0000

    Complete Exercise 1

---
+++ ex01.rb
@@ -0,0 +1,12 @@
+# Exercise 1: A Good First Program
+
+puts "Hello World!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing."
+puts "I'd must rather you 'not'."
+puts 'I "said" do not touch this.'
+
+#Make your script print another line.
+puts "...Here's another line anyway."
