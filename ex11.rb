ab34c072d30710c966e17a2ac522a061934b234b
Sean Haughton <seanphaughton@gmail.com>
2015-01-21 20:45:52 +0000

    Complete Exercise 11

---
+++ ex11.rb
@@ -0,0 +1,21 @@
+# Exercise 11: Asking Questions
+
+print "How old are you? "
+age = gets.chomp
+print "How tall are you? "
+height = gets.chomp
+print "How much do you weigh? "
+weight = gets.chomp
+
+puts "So, you're #{age} old, #{height} tall and #{weight} heavy."
+
+# Write another "form" like this to ask some other Questions
+
+print "What's your name? "
+name = gets.chomp
+print "Where do you live? "
+place = gets.chomp
+print "And how long have you lived there? "
+how_long = gets.chomp
+
+puts "So, your name's #{name}, you live in #{place} and you've lived there #{how_long}."
