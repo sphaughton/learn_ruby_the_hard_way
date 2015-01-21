f2cf2b96265c4608bd3bd15b2e070d235eceb923
Sean Haughton <seanphaughton@gmail.com>
2015-01-21 16:44:15 +0000

    Complete Exercise 10 with Study Drill 3

--- ex10.rb
+++ ex10.rb
@@ -11,7 +11,13 @@ I\'ll do a list:
 \t* Catnip\n\t* Grass
 """
 
+code = "brand new code!"
+
+
 puts tabby_cat
 puts persian_cat
 puts backslash_cat
 puts fat_cat
+
+# Combine escape sequences and format strings to create a more complex format
+puts "Hello\n\tworld!\n\t\tHere\n\t\t\tis\n\t\t\t\tmy\n\t\t\t\t\t#{code}"
