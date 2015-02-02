e5a5334b9439f8cb0115bc1fa5043e806214a06c
Sean Haughton <seanphaughton@gmail.com>
2015-02-02 23:16:07 +0000

    Complete Exercise 34

---
+++ ex34.rb
@@ -0,0 +1,12 @@
+# Exercise 34: Accessing Elements Of Arrays
+
+animals = ['bear', 'python', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+p animals[1] # The second (2nd) animal is at 1 and is a python
+p animals[2] # The third (3rd) animal is at 2 and is a peacock
+p animals[0] # The first (1st) animal is at 0 and is a bear
+p animals[3] # The fourth (4th) animal is at 3 and is a kangaroo
+p animals[4] # The fifth (5th) animal is at 4 and is a whale
+p animals[2] # The third (3rd) animal is at 2 and is a peacock
+p animals[5] # The sixth (6th) animal is at 5 and is a platypus
+p animals[4] # The fifth (5th) animal is at 4 and is a whale
